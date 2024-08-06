4b45d286f97fcf60b370b8221a93b54cd7299f15
Pinata11 <davinnichol@gmail.com>
2024-08-06 20:00:23 +0700

    change in app3 and adding html file

--- JS_Backend/app3.js
+++ JS_Backend/app3.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const fs = require('fs')
 
 let server = http.createServer(function (request, response) {
     if (request.url == '/') {
@@ -6,8 +7,17 @@ let server = http.createServer(function (request, response) {
         response.write('Halaman beranda')
         response.end()
     } else if (request.url == '/profil') {
-        response.writeHead(200, {'Content-Type': 'text/html'})
-        response.write('My profile')
+        fs.createReadStream('./view/profile.html').pipe(response)
+    } else if (request.url == '/news') {
+        response.writeHead(404, {'Content-Type': 'text/html'})
+        response.write(`
+            <h1>Berita saat ini</h1><hr>
+            Judul berita 1 : Lorem ipsum ... <br>
+            Judul berita 2 : Lorem ipsum ... <br>
+            Judul berita 3 : Lorem ipsum ... <br>
+            Judul berita 4 : Lorem ipsum ... <br>
+            Judul berita 5 : Lorem ipsum ... <br>
+            `)
         response.end()
     } else {
         response.writeHead(404, {'Content-Type': 'text/plain'})
