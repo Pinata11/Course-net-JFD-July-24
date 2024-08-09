4b6aa600075b91470b2abfb6c768f85dcc0634ee
Pinata11 <davinnichol@gmail.com>
2024-08-09 21:55:29 +0700

    adding new to profile developer

--- JS_Backend/app-express.js
+++ JS_Backend/app-express.js
@@ -2,24 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => {
-    res.send('<h1>Hello World!</h1>')
+app.set('view engine', 'ejs')
+app.set('views', './view-ejs')
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/about', (req,res) => {
-    res.send('<h1>About Us</h1>')
+app.get('/contact', (req, res) => {
+  let contact = {
+    ig: 'vin_nick1',
+    wa: '08119252563',
+    no: '08119252563',
+    line: 'davinnichol',
+  }
+  res.render('contact', contact)
 })
 
-app.post('/contact', (req,res) => {
-    res.send('<h1>Contact Us</h1>')
+app.get('/profile', (req, res) => {
+  let profile = {
+    role: 'Web Developer',
+    gender: 'Male',
+    gaji: '3000000'
+  }
+  res.render('profile-developer', profile)
 })
 
-app.put('/contact', (req,res) => {
-    res.send('<h1>Contact Us</h1>')
-})
-
-app.delete('/blog/:id', (req,res) => {
-    res.send(`<h1>Blog Post # ${req.params.id} deleted</h1>`)
-})
-
-app.listen(port, () => console.log(`Server is running, open it in http://localhost:3000`))
+app.listen(port, () =>
+  console.log(`Server is running, open it in http://localhost:3000`)
+)
