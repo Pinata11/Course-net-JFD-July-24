234ef4c5dce13bf36767851c5e2afe9c79eba5fa
Pinata11 <davinnichol@gmail.com>
2024-08-09 19:51:59 +0700

    adding nodemon

--- JS_Backend/app-express.js
+++ JS_Backend/app-express.js
@@ -10,12 +10,16 @@ app.get('/about', (req,res) => {
     res.send('<h1>About Us</h1>')
 })
 
-app.get('/contact', (req,res) => {
+app.post('/contact', (req,res) => {
     res.send('<h1>Contact Us</h1>')
 })
 
-app.get('/blog/:id', (req,res) => {
-    res.send(`<h1>Blog Post # ${req.params.id}</h1>`)
+app.put('/contact', (req,res) => {
+    res.send('<h1>Contact Us</h1>')
+})
+
+app.delete('/blog/:id', (req,res) => {
+    res.send(`<h1>Blog Post # ${req.params.id} deleted</h1>`)
 })
 
 app.listen(port, () => console.log(`Server is running, open it in http://localhost:3000`))
