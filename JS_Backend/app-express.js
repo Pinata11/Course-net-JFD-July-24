82386634b47fdb7a6be539b8dced5e107c53adb2
Pinata11 <davinnichol@gmail.com>
2024-08-13 19:32:42 +0700

    adding sql in app-express and detail.ejs

--- JS_Backend/app-express.js
+++ JS_Backend/app-express.js
@@ -76,8 +76,16 @@ app.get('/karyawan/detail/:id_karyawan', async function(req,res) {
 
 
 function get_satuKaryawan(idk) {
+  let sql = `SELECT
+                karyawan.*,
+                department.Code AS code_dept, department.Name AS name_dept,
+                agama.Name AS name_agama
+            FROM karyawan
+            LEFT JOIN department  ON department.ID = karyawan.department_ID
+            LEFT JOIN agama  ON agama.ID = karyawan.agama_ID
+            WHERE karyawan.id = ?`;
   return new Promise( (resolve,reject)=>{
-      db.query("SELECT * FROM karyawan WHERE id = ?", [idk], function(errorSql, hasil) {
+      db.query(sql, [idk], function(errorSql, hasil) {
           if (errorSql) {
               reject(errorSql)
           } else {
