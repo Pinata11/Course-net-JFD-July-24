f86cbe14f3de7a1d5afb36b1090f9cef90037687
Pinata11 <davinnichol@gmail.com>
2024-08-13 18:41:42 +0700

    jfd 12 juli (absent)

--- JS_Backend/app-express.js
+++ JS_Backend/app-express.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql     = require('mysql2')
+const db        = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jfd_belajar_database',
+})
+db.connect()
 
 app.set('view engine', 'ejs')
 app.set('views', './view-ejs')
@@ -28,6 +36,57 @@ app.get('/profile', (req, res) => {
   res.render('profile-developer', profile)
 })
 
+// buat function terpisah untuk
+// proses pengambilan data dari mysql
+function get_semuaKaryawan() {
+  return new Promise( (resolve,reject)=>{
+      db.query("SELECT * FROM karyawan", function(errorSql, hasil) {
+          if (errorSql) {
+              reject(errorSql)
+          } else {
+              resolve(hasil)
+          }
+      })
+  })
+}
+
+
+// gunakan async await, untuk memaksa node js
+// menunggu script yg dipanggil sampai selesai di ekseskusi
+app.get('/karyawan', async function(req,res) {
+  let dataview = {
+      karyawan: await get_semuaKaryawan()
+  }
+  res.render('karyawan/index', dataview)
+})
+
+
+
+app.get('/karyawan/detail/:id_karyawan', async function(req,res) {
+
+  // ambil id yang dikirim via url
+  let idk = req.params.id_karyawan
+
+  // setelah itu kirim ke proses request data mysql
+  let dataview = {
+      pegawai: await get_satuKaryawan(idk),
+  }
+  res.render('karyawan/detail', dataview)
+})
+
+
+function get_satuKaryawan(idk) {
+  return new Promise( (resolve,reject)=>{
+      db.query("SELECT * FROM karyawan WHERE id = ?", [idk], function(errorSql, hasil) {
+          if (errorSql) {
+              reject(errorSql)
+          } else {
+              resolve(hasil)
+          }
+      })
+  })
+}
+
 app.listen(port, () =>
-  console.log(`Server is running, open it in http://localhost:3000`)
+  console.log(`Server is running, open it in http://localhost:` + port)
 )
